171c6e81f8f26ca81742f03639cd1e2ff2aec6ce
eddyiiz <eddyxsayf@gmail.com>
2024-07-18 11:41:17 +0200

    Register page, Userbutton

--- Cashly.Client/Services/AuthService/AuthService.cs
+++ Cashly.Client/Services/AuthService/AuthService.cs
@@ -10,6 +10,7 @@ public class AuthService : IAuthService
     {
         _http = http;
     }
+
     public async Task<ServiceResponse<int>> Register(UserRegister request)
     {
         var result = await _http.PostAsJsonAsync("api/Auth/register", request);
--- Cashly.Shared/UserRegister.cs
+++ Cashly.Shared/UserRegister.cs
@@ -6,7 +6,7 @@ public class UserRegister
 {
     [Required]
     public string Username { get; set; } = string.Empty;
-    [Required, StringLength(100, MinimumLength = 6)]
+    [Required, StringLength(10, MinimumLength = 5)]
     public string Password { get; set; } = string.Empty;
     [Compare("Password", ErrorMessage = "The passwords do not match.")]
     public string ConfirmPassword { get; set; } = string.Empty;
